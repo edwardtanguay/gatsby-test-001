13616823a045fa91e3caed464a56205908c535e9
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-27 23:09:57 +0100

    image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,19 @@ import '../styles/main.scss';
 const IndexPage = () => {
 	return (
 		<>
-	<h1>Info Site</h1>	
-	<ul>
-		<li>first info</li>
-		<li>second info</li>
-	</ul>
+			<h1>Info Site</h1>
+
+			<section>
+				<h2>The Info</h2>
+				<ul>
+					<li>first info</li>
+					<li>second info</li>
+				</ul>
+			</section>
+			<section>
+				<h2>Test Image</h2>
+				<img src="images/test.png" alt="the test" />
+			</section>
 		</>
 	)
 }
